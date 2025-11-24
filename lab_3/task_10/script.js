52502a9e095fccfb2d67e3e344c8e6d8b858d934
yulyaaka <ykeldybay@gmail.com>
2025-11-24 11:28:49 +0000

    19 коммит

---
+++ lab_3/task_10/script.js
@@ -0,0 +1,7 @@
+const list = document.getElementById('my-list');
+
+list.addEventListener('click', event => event.target.tagName === 'LI' && 
+    (event.target.textContent = 'Нажато!', event.target.classList.add('clicked')));
+
+document.getElementById('btn').addEventListener('click', () => 
+    list.insertAdjacentHTML('beforeend', `<li>Элемент ${list.children.length + 1}</li>`));

0cbf8ddab4738eca5e492150a4364a5778687cec
yulyaaka <ykeldybay@gmail.com>
2025-11-26 15:25:55 +0000

    22 коммит 8,10 задачи

--- lab_3/task_10/script.js
+++ lab_3/task_10/script.js
@@ -1,7 +1,12 @@
 const list = document.getElementById('my-list');
 
-list.addEventListener('click', event => event.target.tagName === 'LI' && 
-    (event.target.textContent = 'Нажато!', event.target.classList.add('clicked')));
+list.addEventListener('click', event => {
+    if (event.target.tagName === 'LI') {
+        event.target.textContent = 'Нажато!';
+        event.target.classList.add('clicked');
+    }
+});
 
-document.getElementById('btn').addEventListener('click', () => 
-    list.insertAdjacentHTML('beforeend', `<li>Элемент ${list.children.length + 1}</li>`));
+document.getElementById('btn').addEventListener('click', () => {
+    list.insertAdjacentHTML('beforeend', `<li>Элемент ${list.children.length + 1}</li>`);
+});
--- lab_3/task_8/script.js
+++ lab_3/task_8/script.js
@@ -1,8 +1,10 @@
-function toggleVisibility() {
-    let element = document.getElementById('hiddenDiv');
+const button = document.getElementById('toggleButton');
+const element = document.getElementById('hiddenElement');
+
+button.addEventListener('click', function() {
     if (element.style.display === 'none') {
         element.style.display = 'block';
     } else {
         element.style.display = 'none';
     }
-}
+});

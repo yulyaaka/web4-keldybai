a4b75c6ce5c460ffdc6937238c79a84c187b26da
yulyaaka <ykeldybay@gmail.com>
2025-11-26 15:00:48 +0000

    21 коммит 7 задача

--- lab_3/task_7/script.js
+++ lab_3/task_7/script.js
@@ -1,23 +1,33 @@
 document.getElementById('myForm').addEventListener('submit', function(event) {
-  event.preventDefault(); 
-  const nameInput = document.getElementById('name');
-  const emailInput = document.getElementById('email');
-  const messageDiv = document.getElementById('message');
-
-  if (nameInput.value && emailInput.value) {
-    const formData = {
-      name: nameInput.value,
-      email: emailInput.value
-    };
-
-    messageDiv.textContent = 'Форма успешно отправлена!';
-    messageDiv.style.color = 'green';
-
-    console.log('Данные формы:', formData);
-  } 
-//   else {
-//     console.log('Пожалуйста, заполните все поля формы.');
-//     messageDiv.textContent = 'Пожалуйста, заполните все поля формы.';
-//     messageDiv.style.color = 'red';
-//   }
+  event.preventDefault();
+  const message = document.getElementById('message');
+  const name = document.getElementById('name');
+  const email = document.getElementById('email');
+  
+  message.textContent = '';
+  name.style.border = '';
+  email.style.border = '';
+    
+  if (!name.value) {
+    message.textContent = 'Ошибка: Заполните поле "Имя"';
+    message.style.color = 'red';
+    name.style.border = '2px solid red';
+    return;
+  }
+    
+  if (!email.value) {
+    message.textContent = 'Ошибка: Заполните поле "Email"';
+    message.style.color = 'red';
+    email.style.border = '2px solid red';
+    return;
+  }
+    
+  message.textContent = 'Форма успешно отправлена!';
+  message.style.color = 'green';
+    
+  const formData = new FormData(this);
+  console.log('Данные формы:');
+  for (let [key, value] of formData.entries()) {
+    console.log(`${key}: ${value}`);
+  }
 });
